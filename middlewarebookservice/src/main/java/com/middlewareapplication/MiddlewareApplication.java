1b014592b1e4cce18db7338be3371aa9c901a1e9
Mikhail_Makar <mikhail_makar@epam.com>
2019-12-25 14:01:32 +0300

    Initial commit

---
+++ middlewarebookservice/src/main/java/com/middlewareapplication/MiddlewareApplication.java
@@ -0,0 +1,14 @@
+package com.middlewareapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MiddlewareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiddlewareApplication.class, args);
+    }
+}
