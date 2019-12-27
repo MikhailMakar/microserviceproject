718a01863b99f3679dec2d2ccc9ecbb3e5a3bff1
Mikhail_Makar <mikhail_makar@epam.com>
2019-12-27 14:47:33 +0300

    WIP fixed properties

--- middlewarebookservice/src/main/java/com/middlewareapplication/configuration/MiddlewareBookRouter.java
+++ middlewarebookservice/src/main/java/com/middlewareapplication/configuration/MiddlewareBookRouter.java
@@ -1,7 +1,26 @@
 package com.middlewareapplication.configuration;
 
+import com.middlewareapplication.handler.MiddlewareBookHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RequestPredicates.contentType;
 
 @Configuration
 public class MiddlewareBookRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> route() {
+        MiddlewareBookHandler middlewareBookHandler = new MiddlewareBookHandler();
+        return RouterFunctions
+                .route(GET("/book/{id}").and(accept(APPLICATION_JSON)), middlewareBookHandler::getBook)
+                .andRoute(GET("/book").and(accept(APPLICATION_JSON)), middlewareBookHandler::listBooks)
+                .andRoute(POST("/book/create").and(contentType(APPLICATION_JSON)), middlewareBookHandler::createBook)
+                .andRoute(PUT("book/update").and(contentType(APPLICATION_JSON)), middlewareBookHandler::updateBook);
+    }
 }
--- middlewarebookservice/src/main/java/com/middlewareapplication/handler/MiddlewareBookHandler.java
+++ middlewarebookservice/src/main/java/com/middlewareapplication/handler/MiddlewareBookHandler.java
@@ -1,6 +1,28 @@
 package com.middlewareapplication.handler;
 
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
 public class MiddlewareBookHandler {
 
+    public Mono<ServerResponse> listBooks(ServerRequest request) {
+        return null;
+    }
 
+    public Mono<ServerResponse> getBook(ServerRequest request) {
+        return null;
+    }
+
+    public Mono<ServerResponse> createBook(ServerRequest request) {
+        return null;
+    }
+
+    public Mono<ServerResponse> updateBook(ServerRequest request) {
+        return null;
+    }
 }
